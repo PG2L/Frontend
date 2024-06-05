a9d1cba748db4416a71db7719a1b44fca1e16471
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-05 10:37:04 +0200

    Install Jest and dependancies and set them up

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import NavBar from './NavBar';
 
 it('It should mount', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NavBar />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<NavBar />);
+  root.unmount();
 });
