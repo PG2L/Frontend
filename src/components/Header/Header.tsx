23aadfb88f930dcf558441511bf2e8b9976858a3
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-05 17:51:05 +0200

    chore: Update Header component with shadcn UI components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,62 @@
+"use client"
+
 import React, { FC } from 'react';
 import styles from './Header.module.css';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Input } from '../ui/input';
+
 
 interface HeaderProps { }
 
 const Header: FC<HeaderProps> = () => (
-    <header className={styles.Header} id="header" data-testid="header">
-        <div className="header-left-side">
-            <h1>Header</h1>
-            <div className="header-left-side__menu">
-                <a href="#header" className="menu__link">Home</a>
-                <a href="#about" className="menu__link">About</a>
-                <a href="#contact" className="menu__link">Contact</a>
-            </div>
+    <header className="header flex justify-between items-center px-5 py-3 hover:bg-black/[0.03] border-solid border-b transition-all duration-300" id="header" data-testid="header">
+        <div className="header-left-side flex justify-between items-center space-x-8">
+            <Button asChild variant="outline" className="header-left-side__logo border-solid border border-black px-8 py-2 rounded bg-white/50">
+                <Link href="/">Logo</Link>
+            </Button>
+            <NavigationMenu>
+                <NavigationMenuList className="space-x-6">
+                    <NavigationMenuItem>
+                        <Button asChild variant="ghost">
+                            <NavigationMenuLink href="/">Home</NavigationMenuLink>
+                        </Button>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Button asChild variant="ghost">
+                            <NavigationMenuLink href="/about">About</NavigationMenuLink>
+                        </Button>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Button asChild variant="ghost">
+                            <NavigationMenuLink href="/contact">Contact</NavigationMenuLink>
+                        </Button>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
         </div>
-        <div className="header-right-side">
-            <input type="text" placeholder="Search..." className="search" />
-            <button className="search-button">Search</button>
-            <div className="header-right-side__account">
-                <button className="account__button">Sign in</button>
-                <button className="account__button">Log in</button>
-                <div className="account-dropdown">
-                    <a href="#account" className="dropdown__link">My account</a>
-                    <a href="#logout" className="dropdown__link">Logout</a>
-                </div>
+        <div className="header-right-side flex justify-between items-center space-x-16">
+            <div className="flex w-full max-w-sm items-center space-x-2">
+                <Input type="search" placeholder="Search..." />
+                <Button variant="outline" type="submit">Search</Button>
+            </div>
+            <div className="header-right-side__account flex justify-between items-center space-x-4">
+                <Button asChild variant="outline">
+                    <Link href="/signin">Sign in</Link>
+                </Button>
+                <Button asChild variant="outline">
+                    <Link href="/login">Login</Link>
+                </Button>
             </div>
         </div>
     </header>
