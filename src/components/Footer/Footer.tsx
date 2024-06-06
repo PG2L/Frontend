b5b6c47de63aed573f8c452b091089b12c85e416
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-06 09:02:56 +0200

    Working on Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,66 @@
+"use client"
+
 import React, { FC } from 'react';
 import styles from './Footer.module.css';
+import Link from 'next/link';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from '@/components/ui/navigation-menu';
 
 interface FooterProps { }
 
 const Footer: FC<FooterProps> = () => (
-    <div className={styles.Footer}>
-        Footer Component
-    </div>
+    <footer className="footer flex justify-between py-24 px-36 border-t mt-3">
+        <div className="footer__column flex flex-col items-center justify-center space-y-12">
+            <h3>Column 1</h3>
+            <nav className="footer__column__nav flex flex-col items-center justify-center space-y-6">
+                <Link href="/" className="hover:underline">Alert Dialog</Link>
+                <Link href="/" className="hover:underline">Hover Card</Link>
+                <Link href="/" className="hover:underline">Progress</Link>
+                <Link href="/" className="hover:underline">Scroll-area</Link>
+                <Link href="/" className="hover:underline">Tabs</Link>
+                <Link href="/" className="hover:underline">Tooltip</Link>
+            </nav>
+        </div>
+        <div className="footer__column flex flex-col items-center justify-center space-y-12">
+            <h3>Column 1</h3>
+            <nav className="footer__column__nav flex flex-col items-center justify-center space-y-6">
+                <Link href="/" className="hover:underline">Alert Dialog</Link>
+                <Link href="/" className="hover:underline">Hover Card</Link>
+                <Link href="/" className="hover:underline">Progress</Link>
+                <Link href="/" className="hover:underline">Scroll-area</Link>
+                <Link href="/" className="hover:underline">Tabs</Link>
+                <Link href="/" className="hover:underline">Tooltip</Link>
+            </nav>
+        </div>
+        <div className="footer__column flex flex-col items-center justify-center space-y-12">
+            <h3>Column 1</h3>
+            <nav className="footer__column__nav flex flex-col items-center justify-center space-y-6">
+                <Link href="/" className="hover:underline">Alert Dialog</Link>
+                <Link href="/" className="hover:underline">Hover Card</Link>
+                <Link href="/" className="hover:underline">Progress</Link>
+                <Link href="/" className="hover:underline">Scroll-area</Link>
+                <Link href="/" className="hover:underline">Tabs</Link>
+                <Link href="/" className="hover:underline">Tooltip</Link>
+            </nav>
+        </div>
+        <div className="footer__column flex flex-col items-center justify-center space-y-12">
+            <h3>Column 1</h3>
+            <nav className="footer__column__nav flex flex-col items-center justify-center space-y-6">
+                <Link href="/" className="hover:underline">Alert Dialog</Link>
+                <Link href="/" className="hover:underline">Hover Card</Link>
+                <Link href="/" className="hover:underline">Progress</Link>
+                <Link href="/" className="hover:underline">Scroll-area</Link>
+                <Link href="/" className="hover:underline">Tabs</Link>
+                <Link href="/" className="hover:underline">Tooltip</Link>
+            </nav>
+        </div>
+    </footer >
 );
 
 export default Footer;
