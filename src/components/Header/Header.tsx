0e21e4a03d7a76ac8f3b883f424b3b9d631b1af2
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-06 13:22:07 +0200

    refactor: Update Header component styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -84,85 +84,86 @@ const ListItem = React.forwardRef<
 ListItem.displayName = "ListItem"
 
 const Header: FC<HeaderProps> = () => (
-    <header className="header flex justify-between items-center px-5 py-3 hover:bg-black/[0.03] border-solid border-b transition-all duration-300" id="header" data-testid="header">
-        <div className="header-left-side flex justify-between items-center space-x-8">
-            <Button asChild variant="outline" className="header-left-side__logo border-solid border border-black px-8 py-2 rounded bg-white/50">
-                <Link href="/">Logo</Link>
-            </Button>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                                <li className="row-span-3">
-                                    <NavigationMenuLink asChild>
-                                        <a
-                                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                            href="/"
-                                        >
-                                            {/* <Icons.logo className="h-6 w-6" /> */}
-                                            <div className="mb-2 mt-4 text-lg font-medium">
-                                                shadcn/ui
-                                            </div>
-                                            <p className="text-sm leading-tight text-muted-foreground">
-                                                Beautifully designed components that you can copy and
-                                                paste into your apps. Accessible. Customizable. Open
-                                                Source.
-                                            </p>
-                                        </a>
-                                    </NavigationMenuLink>
-                                </li>
-                                <ListItem href="/docs" title="Introduction">
-                                    Re-usable components built using Radix UI and Tailwind CSS.
-                                </ListItem>
-                                <ListItem href="/docs/installation" title="Installation">
-                                    How to install dependencies and structure your app.
-                                </ListItem>
-                                <ListItem href="/docs/primitives/typography" title="Typography">
-                                    Styles for headings, paragraphs, lists...etc
-                                </ListItem>
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                                {components.map((component) => (
-                                    <ListItem
-                                        key={component.title}
-                                        title={component.title}
-                                        href={component.href}
-                                    >
-                                        {component.description}
+    <header className="header py-3 hover:bg-black/[0.03] border-solid border-b transition-all duration-300" id="header" data-testid="header">
+        <div className="container flex justify-between items-center">
+            <div className="header-left-side flex justify-between items-center space-x-8">
+                <Button asChild variant="outline" className="header-left-side__logo border-solid border border-black px-8 py-2 rounded bg-white/50">
+                    <Link href="/">Logo</Link>
+                </Button>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                    <li className="row-span-3">
+                                        <NavigationMenuLink asChild>
+                                            <a
+                                                className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                                href="/"
+                                            >
+                                                <div className="mb-2 mt-4 text-lg font-medium">
+                                                    shadcn/ui
+                                                </div>
+                                                <p className="text-sm leading-tight text-muted-foreground">
+                                                    Beautifully designed components that you can copy and
+                                                    paste into your apps. Accessible. Customizable. Open
+                                                    Source.
+                                                </p>
+                                            </a>
+                                        </NavigationMenuLink>
+                                    </li>
+                                    <ListItem href="/docs" title="Introduction">
+                                        Re-usable components built using Radix UI and Tailwind CSS.
                                     </ListItem>
-                                ))}
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                Documentation
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-        </div>
-        <div className="header-right-side flex justify-between items-center space-x-16">
-            <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input type="search" placeholder="Search..." />
-                <Button variant="outline" type="submit">Search</Button>
+                                    <ListItem href="/docs/installation" title="Installation">
+                                        How to install dependencies and structure your app.
+                                    </ListItem>
+                                    <ListItem href="/docs/primitives/typography" title="Typography">
+                                        Styles for headings, paragraphs, lists...etc
+                                    </ListItem>
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                    {components.map((component) => (
+                                        <ListItem
+                                            key={component.title}
+                                            title={component.title}
+                                            href={component.href}
+                                        >
+                                            {component.description}
+                                        </ListItem>
+                                    ))}
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <Link href="/docs" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    Documentation
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
             </div>
-            <div className="header-right-side__account flex justify-between items-center space-x-4">
-                <Button asChild variant="outline">
-                    <Link href="/signin">Sign in</Link>
-                </Button>
-                <Button asChild variant="outline">
-                    <Link href="/login">Login</Link>
-                </Button>
+            <div className="header-right-side flex justify-between items-center space-x-16">
+                <div className="flex w-full max-w-sm items-center space-x-2">
+                    <Input type="search" placeholder="Search..." />
+                    <Button variant="outline" type="submit">Search</Button>
+                </div>
+                <div className="header-right-side__account flex justify-between items-center space-x-4">
+                    <Button asChild variant="outline">
+                        <Link href="/signin">Sign in</Link>
+                    </Button>
+                    <Button asChild variant="outline">
+                        <Link href="/login">Login</Link>
+                    </Button>
+                </div>
             </div>
         </div>
     </header>
