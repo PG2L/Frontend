1393797f09b0a41829887fc2608caca4bef96eec
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-06 14:52:05 +0200

    Switch to Pages Router

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "@/components/Layout/Layout";
+import { NextPage } from "next";
+import { useEffect } from "react";
+import React from 'react';
+
+
+export default function Page() {
+    return (
+    <Layout>
+      .lan
+      <div className="container mx-auto">
+        
+      </div>
+    </Layout>
+    )
+  }
+
+// export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config = {
     darkMode: ["class"],
     content: [
         './pages/**/*.{ts,tsx}',
+        './pages/index.{ts,tsx}',
         './components/**/*.{ts,tsx}',
         './app/**/*.{ts,tsx}',
         './src/**/*.{ts,tsx}',
