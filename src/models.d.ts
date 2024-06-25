c8d6cd5ae6b24b9ac633c966bdd9713cd8c294be
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-25 11:12:32 +0200

    Make all properties optionnal

--- src/models.d.ts
+++ src/models.d.ts
@@ -1,65 +1,65 @@
 interface User {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    created_at: string;
-    firstname: string;
-    lastname: string;
-    address: string;
-    country_name: string;
-    total_exp: number;
-    total_points: number;
-    role: 'User' | 'Admin';
-    courses: Course[];
+    id?: number;
+    username?: string;
+    email?: string;
+    password?: string;
+    created_at?: string;
+    firstname?: string;
+    lastname?: string;
+    address?: string;
+    country_name?: string;
+    total_exp?: number;
+    total_points?: number;
+    role?: 'User' | 'Admin';
+    courses?: Course[];
 }
 
 interface Course {
-    id: number;
-    title: string;
-    description: string;
-    points_gain: number;
-    exp_gain: number;
-    difficulty: 'Beginner' | 'Novice' | 'Intermediate' | 'Advanced' | 'Expert' | 'Master' | 'Master+';
-    lessons_count: number;
-    lessons: Lesson[];
-    category: Category;
-    language: Language;
-    is_free: boolean;
-    price: number;
+    id?: number;
+    title?: string;
+    description?: string;
+    points_gain?: number;
+    exp_gain?: number;
+    difficulty?: 'Beginner' | 'Novice' | 'Intermediate' | 'Advanced' | 'Expert' | 'Master' | 'Master+';
+    lessons_count?: number;
+    lessons?: Lesson[];
+    category?: Category;
+    language?: Language;
+    is_free?: boolean;
+    price?: number;
 }
 
 interface Lesson {
-    id: number;
-    title: string;
-    description: string;
-    content: string;
-    points_gain: number;
-    exp_gain: number;
-    lesson_number: number;
-    course: Course;
-    assessment: Assessment;
+    id?: number;
+    title?: string;
+    description?: string;
+    content?: string;
+    points_gain?: number;
+    exp_gain?: number;
+    lesson_number?: number;
+    course?: Course;
+    assessment?: Assessment;
 }
 
 interface Assessment {
-    id: number;
-    title: string;
-    description: string;
-    content: string;
-    lesson: Lesson;
+    id?: number;
+    title?: string;
+    description?: string;
+    content?: string;
+    lesson?: Lesson;
 }
 
 interface Category {
-    id: number;
-    name: string;
+    id?: number;
+    name?: string;
 }
 
 interface Language {
-    id: number;
-    name: string;
+    id?: number;
+    name?: string;
 }
 
 interface Skill {
-    id: number;
-    name: string;
+    id?: number;
+    name?: string;
 }
