22acf025d776c457f733d472db12aeb4bb5ce1c7
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-25 09:01:25 +0200

    Add Charts to home page

--- src/app/(authentified)/home/page.tsx
+++ src/app/(authentified)/home/page.tsx
@@ -3,7 +3,8 @@ import ProfileSidebar from '@/components/ProfileSidebar/ProfileSidebar';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import CourseCard from '@/components/CourseCard/CourseCard';
 import { Calendar } from '@/components/ui/calendar';
-
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import ActivityChart from '@/components/ActivityChart/ActivityChart';
 
 async function getCourses() {
     const response = await fetch('http://localhost:8000/courses')
@@ -23,9 +24,9 @@ export default async function Page() {
             <div className="flex justify-between gap-6 w-3/5">
                 <div className="grid gap-6">
                     <div className="grid">
-                        <h1 className="text-lg">Resume course...</h1>
+                        <h1 className="text-lg">Resume</h1>
                         <ScrollArea>
-                            <div className="w-full mt-2 p-4 flex gap-6 items-center justify-start">
+                            <div className="w-full p-4 flex gap-6 items-center justify-start">
                                 {courses.map((course: any, index: number) => (
                                     <CourseCard key={index} course={course} className="min-w-72" />
                                 ))}
@@ -33,11 +34,17 @@ export default async function Page() {
                             <ScrollBar orientation="horizontal" />
                         </ScrollArea>
                     </div>
-                    <Calendar mode="single" />
+                    <div className="grid">
+                        <h2 className="text-lg">Activity</h2>
+                        <div className="flex">
+                            <Calendar mode="single" />
+                            <ActivityChart />
+                        </div>
+                    </div>
                     <div className="grid">
                         <h1 className="text-lg">Suggestions</h1>
                         <ScrollArea>
-                            <div className="w-full mt-2 flex gap-6 items-center justify-start p-4">
+                            <div className="w-full flex gap-6 items-center justify-start p-4">
                                 {courses.map((course: any, index: number) => (
                                     <CourseCard key={index} course={courses[courses.length - index - 1]} className="min-w-72" />
                                 ))}
