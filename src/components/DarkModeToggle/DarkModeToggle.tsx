971819cbe51a8b85e4a3a602e541c4f4281bc9cc
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-28 15:58:46 +0200

    Add functionnal dark mode toggle switch

---
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -0,0 +1,51 @@
+import React, { FC, useState, useEffect, useMemo } from 'react';
+import { useMediaQuery } from "react-responsive";
+import { Switch } from '../ui/switch';
+import * as icons from 'lucide-react';
+
+interface DarkModeToggleProps { isOpen: boolean; }
+
+const DarkModeToggle: FC<DarkModeToggleProps> = ({
+    isOpen,
+}) => {
+
+    const getTheme = useMemo(() => {
+        return localStorage.getItem('theme') || 'light';
+    },
+        []
+    );
+
+    const [isDark, setIsDark] = useState(getTheme === 'dark');
+
+    const systemPrefersDark = useMediaQuery(
+        {
+            query: "(prefers-color-scheme: dark)",
+        },
+        undefined,
+        (isSystemDark) => setIsDark(isSystemDark)
+    );
+
+    useEffect(() => {
+        if (isDark) {
+            document.body.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    }, [isDark, systemPrefersDark]);
+
+    return (
+        <div className={ `flex gap-2 items-center select-none ${!isOpen && "w-full justify-center"}` }>
+            <Switch
+                checked={ isDark }
+                onCheckedChange={ () => setIsDark(!isDark) }
+                aria-label="Dark mode toggle"
+                id="dark-mode-toggle"
+            />
+            <icons.Moon height={ 20 } width={ 20 } />
+        </div>
+    );
+};
+
+export default DarkModeToggle;
--- src/components/ProfileSidebar/ProfileSidebar.tsx
+++ src/components/ProfileSidebar/ProfileSidebar.tsx
@@ -8,7 +8,8 @@ import { Separator } from '../ui/separator';
 import { Avatar, AvatarImage } from '../ui/avatar';
 import { Progress } from '../ui/progress';
 import { Select, SelectContent, SelectItem, SelectTrigger } from '../ui/select';
-import { Switch } from '../ui/switch';
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
+import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 
 interface ProfileSidebarProps {
     user?: {
@@ -23,22 +24,22 @@ interface ProfileSidebarProps {
 
 const ProfileSidebar: FC<ProfileSidebarProps> = () => {
 
-    const [isOpen, setIsOpen] = React.useState(true);
+    const [isOpen, setIsOpen] = React.useState(false);
 
     return (
-        <>
-            <div className={ `flex bg-card right-0 top-0 sticky z-50 min-h-screen` }>
+        <div>
+            <div className={ `flex bg-card right-0 top-0 sticky z-50 h-screen` }>
                 <div className="flex flex-col items-center h-screen fixed justify-center z-[51]">
                     <Button variant="ghost" onClick={ () => setIsOpen(!isOpen) } className={ `absolute !p-2` } size="sm">
                         { isOpen ? <icons.ChevronRightIcon className="h-6 w-6" /> : <icons.ChevronLeftIcon className="h-6 w-6" /> }
                     </Button>
                 </div>
-                <div className={ `flex flex-col items-start border-l gap-4 justify-between py-6 ${isOpen ? "px-6 sticky" : "px-2 sticky top-0"}` }>
-                    <div className="items-center">
-                        <div className="items-center justify-center flex">
-                            <icons.UserCircleIcon className={ `${isOpen ? "h-20 w-20" : "h-16 w-16"}` } strokeWidth={ 1 } color="#1461cc" />
-                        </div>
-                        { isOpen &&
+                { isOpen ?
+                    <ScrollArea className={ `flex flex-col items-start border-l h-full gap-4 justify-between py-6 px-6 sticky` }>
+                        <div className="items-center">
+                            <div className="items-center justify-center flex">
+                                <icons.UserCircleIcon className={ `h-20 w-20` } strokeWidth={ 1 } color="#1461cc" />
+                            </div>
                             <div className="grid gap-4 justify-items-center">
                                 <div className="grid justify-items-center justify-center">
                                     <div className="grid items-center justify-items-center">
@@ -58,72 +59,66 @@ const ProfileSidebar: FC<ProfileSidebarProps> = () => {
                                     </div>
                                 </div>
                             </div>
-                        }
-                        <Separator className="mt-4" />
-                        <div className={ `mt-4 grid rounded-lg bg-background p-2 border ${isOpen ? '!flex justify-around items-center' : 'justify-items-end items-end gap-2'}` }>
-                            <div className="flex">
-                                39875
-                                <icons.MedalIcon className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                            <Separator className="mt-4" />
+                            <div className={ `mt-4 grid rounded-lg bg-background p-2 border !flex justify-around items-center` }>
+                                <div className="flex">
+                                    39875
+                                    <icons.MedalIcon className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
+                                <div className="flex">
+                                    14085
+                                    <icons.Star className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
+                                <div className="flex">
+                                    14
+                                    <icons.BookCheck className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
                             </div>
-                            <div className="flex">
-                                14085
-                                <icons.Star className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                            <div className="p-4 mt-4 bg-background rounded-lg border w-full grid gap-4">
+                                <div className="flex justify-between items-center">
+                                    <h3 className="text-lg text-center">Next achievement</h3>
+                                    <span className="text-muted-foreground text-center">View all</span>
+                                </div>
+                                <div className="flex gap-4 items-center">
+                                    <Avatar className="h-16 w-16">
+                                        <AvatarImage src="https://avatars.githubusercontent.com/u/242085?v=4" />
+                                    </Avatar>
+                                    <div className="grid gap-2 w-full">
+                                        <span>React Abuser</span>
+                                        <span className="text-muted-foreground">Complete 10 React challenges</span>
+                                        <Progress value={ 92 } />
+                                    </div>
+                                </div>
                             </div>
-                            <div className="flex">
-                                14
-                                <icons.BookCheck className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                            <div className="grid w-full">
+                                <h3 className="text-lg font-medium mt-4">Daily Goals</h3>
+                                {/* <span className="text-muted-foreground text-sm my-2">Complete these challenges daily for even more rewards !</span> */ }
+                                <div className="grid py-2">
+                                    <div className="grid gap-2">
+                                        <div className="flex justify-between items-center">
+                                            <span>Complete 3 lessons</span>
+                                            <span className="text-muted-foreground w-1/3 text-end content-end">+100 XP</span>
+                                        </div>
+                                        <Progress value={ 0 } />
+                                    </div>
+                                    <div className="grid mt-4 gap-2">
+                                        <div className="flex justify-between items-center">
+                                            <span>Complete one lesson without any mistake</span>
+                                            <span className="text-muted-foreground w-1/3 text-end content-end">+50 XP</span>
+                                        </div>
+                                        <Progress value={ 0 } />
+                                    </div>
+                                    <div className="grid mt-4 gap-2">
+                                        <div className="flex justify-between items-center">
+                                            <span>Duel a friend</span>
+                                            <span className="text-muted-foreground w-1/3 text-end content-end">+150 XP</span>
+                                        </div>
+                                        <Progress value={ 0 } />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        { isOpen &&
-                            <>
-                                <div className="p-4 mt-4 bg-background rounded-lg border w-full grid gap-4">
-                                    <div className="flex justify-between items-center">
-                                        <h3 className="text-lg text-center">Next achievement</h3>
-                                        <span className="text-muted-foreground text-center">View all</span>
-                                    </div>
-                                    <div className="flex gap-4 items-center">
-                                        <Avatar className="h-16 w-16">
-                                            <AvatarImage src="https://avatars.githubusercontent.com/u/242085?v=4" />
-                                        </Avatar>
-                                        <div className="grid gap-2 w-full">
-                                            <span>React Abuser</span>
-                                            <span className="text-muted-foreground">Complete 10 React challenges</span>
-                                            <Progress value={ 92 } />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="grid w-full">
-                                    <h3 className="text-lg font-medium mt-4">Daily Goals</h3>
-                                    {/* <span className="text-muted-foreground text-sm my-2">Complete these challenges daily for even more rewards !</span> */ }
-                                    <div className="grid py-2">
-                                        <div className="grid gap-2">
-                                            <div className="flex justify-between items-center">
-                                                <span>Complete 3 lessons</span>
-                                                <span className="text-muted-foreground w-1/3 text-end content-end">+100 XP</span>
-                                            </div>
-                                            <Progress value={ 0 } />
-                                        </div>
-                                        <div className="grid mt-4 gap-2">
-                                            <div className="flex justify-between items-center">
-                                                <span>Complete one lesson without any mistake</span>
-                                                <span className="text-muted-foreground w-1/3 text-end content-end">+50 XP</span>
-                                            </div>
-                                            <Progress value={ 0 } />
-                                        </div>
-                                        <div className="grid mt-4 gap-2">
-                                            <div className="flex justify-between items-center">
-                                                <span>Duel a friend</span>
-                                                <span className="text-muted-foreground w-1/3 text-end content-end">+150 XP</span>
-                                            </div>
-                                            <Progress value={ 0 } />
-                                        </div>
-                                    </div>
-                                </div>
-                            </>
-                        }
-                    </div>
-                    <div className="grid gap-4 w-full">
-                        { isOpen &&
+                        <div className="grid gap-4 w-full">
                             <div className="bg-background rounded-lg border p-4 grid gap-4">
                                 <div className="flex justify-between items-center">
                                     <h3 className="text-lg text-center">Friends <span className="text-base text-muted-foreground">(All-time XP)</span></h3>
@@ -157,28 +152,64 @@ const ProfileSidebar: FC<ProfileSidebarProps> = () => {
                                     </div>
                                 </div>
                             </div>
-                        }
-                        <div className={ `items-center w-full flex ${isOpen ? "justify-between" : "flex-col gap-6"}` }>
-                            <div className={ `flex gap-2 items-center ${!isOpen && "w-full justify-center"}` }>
-                                <Switch id="dark-mode-toggle" aria-orientation='vertical' />
-                                <icons.Moon height={ 20 } width={ 20 } />
+                            <div className={ `items-center w-full flex justify-between` }>
+                                <DarkModeToggle isOpen={ isOpen } />
+                                <Select>
+                                    <SelectTrigger className="w-fit flex gap-2">
+                                        <icons.FlagIcon />
+                                        Français
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="eng">English</SelectItem>
+                                        <SelectItem value="fr">Français</SelectItem>
+                                    </SelectContent>
+                                </Select>
                             </div>
-                            <Select>
-                                <SelectTrigger className="w-fit flex gap-2">
-                                    <icons.FlagIcon />
-                                    { isOpen && "Français" }
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem value="eng">English</SelectItem>
-                                    <SelectItem value="fr">Français</SelectItem>
-                                </SelectContent>
-                            </Select>
+                            <Separator />
                         </div>
-                        <Separator />
+                        <ScrollBar />
+                    </ScrollArea>
+                    :
+                    <div className="items-center flex flex-col justify-between py-6 px-2 border-l">
+                        <div>
+                            <div className="items-center justify-center flex">
+                                <icons.UserCircleIcon className={ `h-16 w-16` } strokeWidth={ 1 } color="#1461cc" />
+                            </div>
+                            <Separator className="mt-4" />
+                            <div className={ `mt-4 grid rounded-lg bg-background p-2 border ${isOpen ? '!flex justify-around items-center' : 'justify-items-end items-end gap-2'}` }>
+                                <div className="flex">
+                                    39875
+                                    <icons.MedalIcon className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
+                                <div className="flex">
+                                    14085
+                                    <icons.Star className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
+                                <div className="flex">
+                                    14
+                                    <icons.BookCheck className="ms-1 h-6 w-6 text-primary" strokeWidth={ 1 } />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={ `items-center w-full grid gap-4` }>
+                                <DarkModeToggle isOpen={ isOpen } />
+                                <Select>
+                                    <SelectTrigger className="w-fit flex gap-2">
+                                        <icons.FlagIcon />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="eng">English</SelectItem>
+                                        <SelectItem value="fr">Français</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                                <Separator />
+                            </div>
+                        </div>
                     </div>
-                </div>
+                }
             </div>
-        </>
+        </div>
     );
 };
 
