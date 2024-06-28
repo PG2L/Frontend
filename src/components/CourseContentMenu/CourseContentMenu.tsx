2768909041fff110a618d74b6a99adc787e05d8b
Cyprien De Roef <c.deroef@outlook.fr>
2024-06-28 16:00:26 +0200

    Linter

--- src/components/CourseContentMenu/CourseContentMenu.tsx
+++ src/components/CourseContentMenu/CourseContentMenu.tsx
@@ -27,43 +27,43 @@ const CourseContentMenu: FC<CourseContentMenuProps> = ({
     return (
         <>
             <div className="grid h-full gap-6">
-                <nav className="px-6 mt-6 lg:sticky top-6 h-fit grid gap-6">
+                <nav className="px-6 mt-6 top-6 h-fit grid gap-6">
                     <Separator />
                     <ul className="grid gap-2">
-                        {courseContent.lessons && courseContent.lessons.map((lesson, index) => (
-                            <Suspense key={index} fallback={
+                        { courseContent.lessons && courseContent.lessons.map((lesson, index) => (
+                            <Suspense key={ index } fallback={
                                 <Skeleton className="w-full h-6"></Skeleton>
                             }>
                                 <li>
-                                    <Link href={`/courses/${params.courseId}/${lesson.id}`}>
-                                        <Button variant="ghost" className={`text-muted-foreground w-full text-start font-normal text-wrap ${(lesson.id == params.lessonId) && "active"}`}>
-                                            <h2 className="w-full">{index + 1} .  {lesson.title}</h2>
+                                    <Link href={ `/courses/${params.courseId}/${lesson.id}` }>
+                                        <Button variant="ghost" className={ `text-muted-foreground w-full text-start font-normal text-wrap ${(lesson.id == params.lessonId) && "active"}` }>
+                                            <h2 className="w-full">{ index + 1 } .  { lesson.title }</h2>
                                         </Button>
                                     </Link>
                                 </li>
                             </Suspense>
-                        ))}
+                        )) }
                     </ul>
                     <Separator />
-                    {params.hasOwnProperty('lessonId') ?
-                        <div className={`flex gap-6 justify-between ${isFirstLesson && "!justify-end"}`}>
-                            {!isFirstLesson &&
-                                <Link href={`/courses/${params.courseId}/${params.lessonId - 1}`} className="justify-self-start">
+                    { params.hasOwnProperty('lessonId') ?
+                        <div className={ `flex gap-6 justify-between ${isFirstLesson && "!justify-end"}` }>
+                            { !isFirstLesson &&
+                                <Link href={ `/courses/${params.courseId}/${params.lessonId - 1}` } className="justify-self-start">
                                     <Button variant="ghost">
-                                        <icons.ChevronsLeft strokeWidth={1} />
+                                        <icons.ChevronsLeft strokeWidth={ 1 } />
                                     </Button>
                                 </Link>
                             }
-                            {!isLastLesson &&
-                                <Link href={`/courses/${params.courseId}/${parseInt(params.lessonId) + 1}`} className="justify-self-end">
+                            { !isLastLesson &&
+                                <Link href={ `/courses/${params.courseId}/${parseInt(params.lessonId) + 1}` } className="justify-self-end">
                                     <Button variant="ghost">
-                                        <icons.ChevronsRight strokeWidth={1} />
+                                        <icons.ChevronsRight strokeWidth={ 1 } />
                                     </Button>
                                 </Link>
                             }
                         </div>
                         :
-                        <Link href={`/courses/${params.courseId}/${courseContent.lessons[0].id}`} className="justify-self-center w-1/2">
+                        <Link href={ `/courses/${params.courseId}/${courseContent.lessons[0].id}` } className="justify-self-center w-1/2">
                             <Button className="w-full">
                                 Start Course
                             </Button>
@@ -72,7 +72,7 @@ const CourseContentMenu: FC<CourseContentMenuProps> = ({
                 </nav>
             </div>
         </>
-    )
+    );
 };
 
 export default CourseContentMenu;
