081b8403255068edabd5633411392740c8bb7d03
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-09 11:16:44 +0200

    Fix breadcrumb in /courses

--- app/_components/GlobalBreadcrumb/GlobalBreadcrumb.tsx
+++ app/_components/GlobalBreadcrumb/GlobalBreadcrumb.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState, useContext } from 'react';
 import styles from './GlobalBreadcrumb.module.css';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from '../ui/breadcrumb';
+import { CourseContext } from '../../_contexts/CourseProvider';
 
 interface GlobalBreadcrumbProps {
     courses?: Course[],
@@ -18,36 +19,28 @@ const GlobalBreadcrumb: FC<GlobalBreadcrumbProps> = ({
 
     const paths: string = usePathname() || '';
     const pathNames: string[] = paths.split('/').filter(path => path);
-    const [itemLinks, setItemLinks] = useState<string[]>([]);
-
-    useEffect((): void => {
-        const fetchItemsNames: () => Promise<void> = async (): Promise<void> => {
-            const links: string[] = await Promise.all(pathNames.map(async (link: string, index: number): Promise<any> => {
-                if (link.match(/^[0-9]+$/)) {
-                    if (index === 1) {
-                        return courses[parseInt(link) - 1].title;
-                    } else if (index === 2) {
-                        return courses[parseInt(pathNames[1]) - 1].lessons.find((lesson: any): boolean => lesson.id === parseInt(link)).title;
-                    }
-                }
-                return link[0].toUpperCase() + link.slice(1, link.length);
-            }));
-            setItemLinks(links);
-        };
-        fetchItemsNames();
-    }, [pathNames]);
+    const itemsLinks: string[] = pathNames.map((link: string, index: number): string => {
+        if (link.match(/^[0-9]+$/)) {
+            if (index === 1) {
+                return courses[parseInt(link) - 1].title;
+            } else if (index === 2) {
+                return courses[parseInt(pathNames[1]) - 1].lessons.find((lesson: any): boolean => lesson.id === parseInt(link)).title;
+            }
+        }
+        return link[0].toUpperCase() + link.slice(1, link.length);
+    });
 
     return (
         <>
             { pathNames.length > 1 &&
-                <nav className={ styles.breadcrumb }>
+                <nav>
                     <Breadcrumb>
                         <BreadcrumbList>
-                            { itemLinks.map((itemLink: string, index: number): React.JSX.Element => {
+                            { itemsLinks.map((itemLink: string, index: number): React.JSX.Element => {
                                 let href: string = `/${pathNames.slice(0, index + 1).join('/')}`;
                                 return (
                                     <>
-                                        { itemLinks.length !== index + 1 ?
+                                        { itemsLinks.length !== index + 1 ?
                                             <>
                                                 <BreadcrumbItem key={ index }>
                                                     <BreadcrumbLink asChild>
