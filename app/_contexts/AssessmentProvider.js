52db6700ae16d4e6f3f142fc0af044dda2e3de98
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-13 10:18:30 +0200

    Init AssessmentProvider.js

---
+++ app/_contexts/AssessmentProvider.js
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { createContext } from "react"
+
+/**
+ * @fileoverview Defines the AssessmentContext for the application.
+ * @module AssessmentProvider
+ */
+export const AssessmentContext = createContext();
+
+/**
+ * Provides the assessment context to its children components.
+ *
+ * @param {Object} props - The component props.
+ * @param {ReactNode} props.children - The child components.
+ * @param {Object} props.assessment - The assessment object.
+ * @returns {ReactNode} The rendered component.
+ */
+export default function AssessmentProvider({ children, assessment }) {
+
+    return (
+
+        <AssessmentContext.Provider value={assessment} >
+            { children }
+        </AssessmentContext.Provider>
+
+    )
+}
