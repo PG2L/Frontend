89dcd267bae6e4fbc16b3d948ee536f3800a9c6c
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-16 17:32:07 +0200

    Init /achievements

---
+++ app/(authentified)/achievements/loading.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Skeleton } from '@/_components/ui/skeleton';
+
+/**
+ * Renders a loading component with skeleton placeholders.
+ *
+ * @returns The JSX element representing the loading component.
+ */
+export default function Loading(): React.JSX.Element {
+
+    return (
+
+        <div className="grid items-center justify-center grid-cols-1 gap-6">
+            <h1 className="text-xl font-medium text-center">Achievements</h1>
+            <div className="grid gap-4">
+                <p>Welcome to the Achievements page! Here, you can track and celebrate your milestones in our web development e-learning community. Achievements are designed to recognize your hard work and dedication as you progress through various courses and challenges.</p>
+                <p className="text-muted-foreground">Earn badges, complete projects, and participate in discussions to unlock new achievements. Each milestone you reach is a testament to your commitment to learning and growing as a web developer.</p>
+                <p>Keep pushing forward, and showcase your accomplishments proudly!</p>
+                <Skeleton className="h-10 w-1/4 mt-4" />
+                <div>
+                    { Array.from({ length: 10 }).map((_: unknown, index: number): React.JSX.Element => ( // Mapping over an array of 10 undefined elements to render 10 skeleton placeholders.
+                        <Skeleton key={ index } className="h-16 w-full mt-2" />
+                    )) }
+                </div>
+            </div>
+        </div>
+
+    );
+}
---
+++ app/(authentified)/achievements/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import AchievementsTable from '@/_components/AchievementsTable/AchievementsTable';
+import { getData } from '@/_lib/data';
+
+/**
+ * Renders the Achievement page component.
+ * 
+ * @returns A Promise that resolves to a JSX.Element representing the Achievement page.
+ */
+export default async function Page(): Promise<React.JSX.Element> {
+
+    /**
+     * Retrieves the achievements data from the server.
+     * 
+     * @returns {Promise<Achievement[]>} A promise that resolves to an array of Achievement objects.
+     */
+    const achievements: Achievement[] = await getData("achievements") as Achievement[];
+
+    return (
+
+        <div className="grid items-center justify-center grid-cols-1 gap-6">
+            <h1 className="text-xl font-medium text-center">Achievements</h1>
+            <div className="grid gap-4">
+                <p>Welcome to the Achievements page! Here, you can track and celebrate your milestones in our web development e-learning community. Achievements are designed to recognize your hard work and dedication as you progress through various courses and challenges.</p>
+                <p className="text-muted-foreground">Earn badges, complete projects, and participate in discussions to unlock new achievements. Each milestone you reach is a testament to your commitment to learning and growing as a web developer.</p>
+                <p>Keep pushing forward, and showcase your accomplishments proudly!</p>
+            </div>
+            <AchievementsTable achievements={ achievements } />
+        </div>
+
+    );
+}
