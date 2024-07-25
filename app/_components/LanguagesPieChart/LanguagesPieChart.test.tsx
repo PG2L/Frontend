a808964e8f78d3eb82a75dd6f5c2be3c5cc7537a
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-25 12:54:55 +0200

    Removed LanguagesPieChart to rebuild it

--- app/_components/LanguagesPieChart/LanguagesPieChart.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LanguagesPieChart from './LanguagesPieChart';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LanguagesPieChart />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
